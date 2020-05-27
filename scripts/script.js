216de0c381d378e2f92ad7ca4b12c0ea1aaf7655
swach2 <me@sonyawach.com>
2020-05-27 16:55:18 -0700

    feat: add card elements and functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 // Modal open and close
-const modal = document.querySelector(".popup");
-const modalopen = document.querySelector(".edit-button")
-const modalclose = document.querySelector(".close-button");
-const submitbtn = document.querySelector(".save-button");
+const modal_profile = document.querySelector(".profile-popup");
+const modalopen_profile = document.querySelector(".edit-button")
+const modalclose_profile = document.querySelector(".profile-close");
+const submitbtn_profile = document.querySelector(".profile-save");
 
-modalopen.addEventListener("click", function () {
-  modal.classList.add("popup_opened");
+modalopen_profile.addEventListener("click", function () {
+  modal_profile.classList.add("popup_opened");
 });
 
-modalclose.addEventListener("click", function () {
-  modal.classList.remove("popup_opened");
+modalclose_profile.addEventListener("click", function () {
+  modal_profile.classList.remove("popup_opened");
 });
 
-submitbtn.addEventListener("click", function () {
-  modal.classList.remove("popup_opened");
+submitbtn_profile.addEventListener("click", function () {
+  modal_profile.classList.remove("popup_opened");
 });
 
 // Save form input
-const formElement =  document.querySelector(".popup__form");
+const profileformElement =  document.querySelector(".profile-form");
 
 // Next is the form submit handler, though
 // it won't submit anywhere just yet
@@ -27,8 +27,8 @@ function formSubmitHandler (evt) {
                                                 // We'll explain it in more detail later.
 
     // Let's find the form fields in the DOM
-    const nameInput = document.querySelector('input[name="profilename"]');
-    const jobInput = document.querySelector('input[name="profiletitle"]');
+    const nameInput = document.querySelector('input[name="profile-name"]');
+    const jobInput = document.querySelector('input[name="profile-title"]');
 
 
     // Get the values of each field from the corresponding value property
@@ -46,4 +46,132 @@ function formSubmitHandler (evt) {
 
 // Connect the handler to the form:
 // it will watch the submit event
-formElement.addEventListener('submit', formSubmitHandler);
+profileformElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanois National Park",
+        link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+const elementList = document.querySelector(".elements__list");
+const imagemodal = document.querySelector(".image-popup");
+const imageclose = document.querySelector(".image-popup__close");
+const imagecontainer = document.querySelector(".image-popup__image");
+const imagetitle = document.querySelector(".image-popup__title");
+
+function initialCard() {
+  const elementTemplate = document.querySelector("#element").content;
+
+  for (var i = 0; i < initialCards.length; i++) {
+    var cardinfo = initialCards[i];
+    const itemElement = elementTemplate.cloneNode(true);
+    itemElement.querySelector(".element").style.backgroundImage =  "url('" + cardinfo.link + "')";
+    itemElement.querySelector(".element__title").textContent = cardinfo.name;
+
+    itemElement.querySelector(".delete-button").addEventListener("click", function (evt) {
+        evt.target.closest(".element").remove();
+    });
+
+    itemElement.querySelector(".heart-button_inactive").addEventListener("click", function (evt) {
+        evt.target.classList.toggle('heart-button_active');
+    });
+
+////////////////
+    itemElement.querySelector(".element__click").addEventListener("click", function (evt) {
+        imagemodal.classList.add("popup_opened");
+        imagecontainer.src = evt.target.parentElement.style.backgroundImage.replace('url("','').replace('")','');
+        // imagecontainer.style.backgroundImage = evt.target.parentElement.style.backgroundImage;
+        //console.log(evt.target.parentElement.innerText);
+        imagetitle.textContent = evt.target.parentElement.innerText;
+    });
+
+    imageclose.addEventListener("click", function () {
+        imagemodal.classList.remove("popup_opened");
+    });
+
+//////////////
+    elementList.appendChild(itemElement);
+  }
+
+}
+
+window.onload = initialCard();
+
+function newCard(newcardTitle, newcardURL) {
+  const elementTemplate = document.querySelector("#element").content;
+  const itemElement = elementTemplate.cloneNode(true);
+
+  itemElement.querySelector(".element").style.backgroundImage =  "url('" + newcardURL + "')";
+  itemElement.querySelector(".element__title").textContent = newcardTitle;
+
+  itemElement.querySelector(".delete-button").addEventListener("click", function (evt) {
+      evt.target.closest(".element").remove();
+  });
+
+  itemElement.querySelector(".heart-button").addEventListener("click", function (evt) {
+      evt.target.classList.toggle('heart-button_active');
+  });
+
+  elementList.prepend(itemElement);
+}
+
+const modal_newitem = document.querySelector(".newitem");
+const modalopen_newitem = document.querySelector(".add-button");
+const modalclose_newitem = document.querySelector(".newitem__close");
+const submitbtn_newitem = document.querySelector(".newitem__save");
+
+modalopen_newitem.addEventListener("click", function () {
+  modal_newitem.classList.add("popup_opened");
+});
+
+modalclose_newitem.addEventListener("click", function () {
+  modal_newitem.classList.remove("popup_opened");
+});
+
+submitbtn_newitem.addEventListener("click", function () {
+  modal_newitem.classList.remove("popup_opened");
+});
+
+const newitemformElement =  document.querySelector(".newitem__form");
+
+function newitemformSubmitHandler (evt) {
+    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
+                                                // Having done so, we can define our own way of submitting the form.
+                                                // We'll explain it in more detail later.
+
+    // Let's find the form fields in the DOM
+    const titleInput = document.querySelector('input[name="newitem-title"]');
+    const urlInput = document.querySelector('input[name="newitem-url"]');
+
+
+    // Get the values of each field from the corresponding value property
+    const newitemTitle = titleInput.value;
+    const newitemURL = urlInput.value;
+
+    newCard(newitemTitle, newitemURL);
+
+}
+
+newitemformElement.addEventListener('submit', newitemformSubmitHandler);
